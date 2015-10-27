ea7dcd4e1f418a1ec15b1b86068a803fc0c36332
wancheng <wancheng.com.cn@gmail.com>
2015-10-27 18:30:28 +0800

    init script

--- db/init.py
+++ db/init.py
@@ -1,13 +1,22 @@
 import sqlite3
+import hashlib
 
 conn = sqlite3.connect('piki.db')
 cur = conn.cursor()
+cur.execute("""
+    CREATE TABLE piki_user (id INTEGER primary key,username text,password text,realname text,email text)
+    """);
 
-f = open('user.db')
-for l in f.readlines():
-    line = l.split(' @@ ')
-    cur.execute("insert into piki_user (username,realname,password,email) values (?,?,?,?)",(line[2],line[1],line[3],line[4]))
+cur.execute("""
+    CREATE TABLE piki_wiki (id INTEGER primary key,title TEXT,markdown TEXT,creator TEXT,create_time TEXT,pv INTEGER)
+    """);
 
-f.close()
+cur.execute("""
+    CREATE TABLE piki_subscription(id INTEGERY primary key,uid INTEGERY,wid INTEGERY,create_time text)
+    """);
+
+passwd = hashlib.md5('admin'.encode()).hexdigest() 
+cur.execute("insert into piki_user (username,realname,password,email) values (?,?,?,?)",('admin','admin',passwd,'admin@test.com'))
+
 conn.commit()
 conn.close()
